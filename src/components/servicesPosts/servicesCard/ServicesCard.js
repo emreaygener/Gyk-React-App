4a5ae377a9268588eb1b2ad531da24532866d425
Emre Aygener <emre.aygener@gmail.com>
2024-08-09 20:03:19 +0300

    live review#13fix#1loadingAnimationAdded

--- src/components/servicesPosts/servicesCard/ServicesCard.js
+++ src/components/servicesPosts/servicesCard/ServicesCard.js
@@ -1,8 +1,13 @@
 import "./ServicesCard.css";
 export default function ServicesCard({ service }) {
   const backgroundImage = `url(${service.image})`;
+  const delayFactor = (service.id % 23) - 1;
+  const animationDelay = `${delayFactor * 0.2}s`;
   return (
-    <article className="services-card" style={{ backgroundImage }}>
+    <article
+      className="services-card"
+      style={{ backgroundImage, animationDelay }}
+    >
       <section className="services-text">
         <h3>{service.title}</h3>
         <p>{service.content}</p>
