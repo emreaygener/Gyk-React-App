0058fa7d5ff4bb37bc954c7bc3c24802fabef863
Emre Aygener <emre.aygener@gmail.com>
2024-09-17 19:06:21 +0300

    Some Style fixes

--- src/components/clipboard/Clipboard.js
+++ src/components/clipboard/Clipboard.js
@@ -1,4 +1,4 @@
-import BlogCard from "./blogCard/BlogCard";
+import BlogCard from "../blog/blogCard/BlogCard";
 import DataFetcher from "../../util/DataFetcher";
 import "./Clipboard.css";
 
@@ -10,38 +10,56 @@ export default function Clipboard() {
     {
       id: 1,
       title: "Blog Post 1",
-      content: "This is the content of the first blog post.",
-      author: "Author",
+      author: "John Doe",
+      summary: "This is the first blog post.",
+      content:
+        "This is the first blog post. It is very interesting. There are many things to learn from it. so, read it carefully. And enjoy it.",
+      image: "https://picsum.photos/300/300/?random",
     },
     {
       id: 2,
       title: "Blog Post 2",
-      content: "This is the content of the second blog post.",
-      author: "Author",
+      author: "Jane Doe",
+      summary: "This is the second blog post.",
+      content:
+        "This is the second blog post. It is very interesting. There are many things to learn from it. so, read it carefully. And enjoy it.",
+      image: "https://picsum.photos/300/300/?randomid=2",
     },
     {
       id: 3,
       title: "Blog Post 3",
-      content: "This is the content of the third blog post.",
-      author: "Author",
+      author: "John Doe",
+      summary: "This is the third blog post.",
+      content:
+        "This is the third blog post. It is very interesting. There are many things to learn from it. so, read it carefully. And enjoy it.",
+      image: "https://picsum.photos/300/300/?randomid=3",
     },
     {
       id: 4,
       title: "Blog Post 4",
-      content: "This is the content of the fourth blog post.",
-      author: "Author",
+      author: "Jane Doe",
+      summary: "This is the fourth blog post.",
+      content:
+        "This is the fourth blog post. It is very interesting. There are many things to learn from it. so, read it carefully. And enjoy it.",
+      image: "https://picsum.photos/300/300/?randomid=4",
     },
     {
       id: 5,
       title: "Blog Post 5",
-      content: "This is the content of the fifth blog post.",
-      author: "Author",
+      author: "John Doe",
+      summary: "This is the fifth blog post.",
+      content:
+        "This is the fifth blog post. It is very interesting. There are many things to learn from it. so, read it carefully. And enjoy it.",
+      image: "https://picsum.photos/300/300/?randomid=5",
     },
     {
       id: 6,
       title: "Blog Post 6",
-      content: "This is the content of the sixth blog post.",
-      author: "Author",
+      author: "Jane Doe",
+      summary: "This is the sixth blog post.",
+      content:
+        "This is the sixth blog post. It is very interesting. There are many things to learn from it. so, read it carefully. And enjoy it.",
+      image: "https://picsum.photos/300/300/?randomid=6",
     },
   ];
 
@@ -70,7 +88,7 @@ export default function Clipboard() {
         <menu id="blog-menu">
           {data.map((card) => (
             <article key={card.id}>
-              <BlogCard data={card} />
+              <BlogCard post={card} />
             </article>
           ))}
         </menu>
